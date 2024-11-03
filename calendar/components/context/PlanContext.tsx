41272c963bd7691fd68a08814ac58a6640c9fd3c
sousou1216 <sousou1216@yahoo.co.jp>
2024-11-03 22:01:01 +0900

    モジュール名の修正等のリファクタ

---
+++ calendar/components/context/PlanContext.tsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react';
+
+export const PlanContext = createContext();
+
+export const PlanProvider = ({ children }) => {
+  const [plan, setPlan] = useState([]);
+
+  return (
+    <PlanContext.Provider value={{ plan, setPlan }}>
+      {children}
+    </PlanContext.Provider>
+  );
+};
